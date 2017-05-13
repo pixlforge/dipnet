b2a209bb3b5523665aa3a4a3f3e49bea7830c5e8
Pixlforge <admin@pixlforge.ch>
2017-05-13 06:14:56 +0200

    Modifié les routes en utilisant le singulier du controller

--- routes/web.php
+++ routes/web.php
@@ -3,137 +3,126 @@
 /**
  * Basic routes
  */
-Route::get('/', 'PagesController@welcome');
-Route::get('/index', 'PagesController@index')->name('home');
+Route::get('/', 'PageController@welcome');
+Route::get('/index', 'PageController@index')->name('home');
 
 /**
  * User routes
  */
-Route::get('/users', 'UsersController@index');
-Route::get('/users/create', 'UsersController@create');
-Route::post('/users', 'UsersController@store');
-Route::get('/users/{user}', 'UsersController@show');
-Route::get('/users/{user}/edit', 'UsersController@edit');
-Route::patch('/users/{user}', 'UsersController@update');
-Route::delete('/users/{user}', 'UsersController@destroy');
+Route::get('/user', 'UserController@index');
+Route::get('/user/create', 'UserController@create');
+Route::post('/user', 'UserController@store');
+Route::get('/user/{user}', 'UserController@show');
+Route::get('/user/{user}/edit', 'UserController@edit');
+Route::patch('/user/{user}', 'UserController@update');
+Route::delete('/user/{user}', 'UserController@destroy');
 
 /**
  * Business routes
  */
-Route::get('/businesses', 'BusinessesController@index');
-Route::get('/businesses/create', 'BusinessesController@create');
-Route::post('/businesses', 'BusinessesController@store');
-Route::get('/businesses/{business}', 'BusinessesController@show');
-Route::get('/businesses/{business}/edit', 'BusinessesController@edit');
-Route::patch('/businesses/{business}', 'BusinessesController@update');
-Route::delete('/businesses/{business}', 'BusinessesController@destroy');
+Route::get('/business', 'BusinessController@index');
+Route::get('/business/create', 'BusinessController@create');
+Route::post('/business', 'BusinessController@store');
+Route::get('/business/{business}', 'BusinessController@show');
+Route::get('/business/{business}/edit', 'BusinessController@edit');
+Route::patch('/business/{business}', 'BusinessController@update');
+Route::delete('/business/{business}', 'BusinessController@destroy');
 
 /**
  * Company routes
  */
-Route::get('/companies', 'CompaniesController@index');
-Route::get('/companies/create', 'CompaniesController@create');
-Route::post('/companies', 'CompaniesController@store');
-Route::get('/companies/{company}', 'CompaniesController@show');
-Route::get('/companies/{company}/edit', 'CompaniesController@edit');
-Route::patch('/companies/{company}', 'CompaniesController@update');
-Route::delete('/companies/{company}', 'CompaniesController@destroy');
+Route::get('/company', 'CompanyController@index');
+Route::get('/company/create', 'CompanyController@create');
+Route::post('/company', 'CompanyController@store');
+Route::get('/company/{company}', 'CompanyController@show');
+Route::get('/company/{company}/edit', 'CompanyController@edit');
+Route::patch('/company/{company}', 'CompanyController@update');
+Route::delete('/company/{company}', 'CompanyController@destroy');
 
 /**
  * Contact routes
  */
-Route::get('/contacts', 'ContactsController@index');
-Route::get('/contacts/create', 'ContactsController@create');
-Route::post('/contacts', 'ContactsController@store');
-Route::get('/contacts/{contact}', 'ContactsController@show');
-Route::get('/contacts/{contact}/edit', 'ContactsController@edit');
-Route::patch('/contacts/{contact}', 'ContactsController@update');
-Route::delete('/contacts/{contact}', 'ContactsController@destroy');
+Route::get('/contact', 'ContactController@index');
+Route::get('/contact/create', 'ContactController@create');
+Route::post('/contact', 'ContactController@store');
+Route::get('/contact/{contact}', 'ContactController@show');
+Route::get('/contact/{contact}/edit', 'ContactController@edit');
+Route::patch('/contact/{contact}', 'ContactController@update');
+Route::delete('/contact/{contact}', 'ContactController@destroy');
 
 /**
  * Order routes
  */
-Route::get('/orders', 'OrdersController@index');
-Route::get('/orders/create', 'OrdersController@create');
-Route::post('/orders', 'OrdersController@store');
-Route::get('/orders/{order}', 'OrdersController@show');
-Route::get('/orders/{order}/edit', 'OrdersController@edit');
-Route::patch('/orders/{order}', 'OrdersController@update');
-Route::delete('/orders/{order}', 'OrdersController@destroy');
+Route::get('/order', 'OrderController@index');
+Route::get('/order/create', 'OrderController@create');
+Route::post('/order', 'OrderController@store');
+Route::get('/order/{order}', 'OrderController@show');
+Route::get('/order/{order}/edit', 'OrderController@edit');
+Route::patch('/order/{order}', 'OrderController@update');
+Route::delete('/order/{order}', 'OrderController@destroy');
 
 /**
  * Format routes
  */
-Route::get('/formats', 'FormatsController@index');
-Route::get('/formats/create', 'FormatsController@create');
-Route::post('/formats', 'FormatsController@store');
-Route::get('/formats/{format}', 'FormatsController@show');
-Route::get('/formats/{format}/edit', 'FormatsController@edit');
-Route::patch('/formats/{format}', 'FormatsController@update');
-Route::delete('/formats/{format}', 'FormatsController@destroy');
+Route::get('/format', 'FormatController@index');
+Route::get('/format/create', 'FormatController@create');
+Route::post('/format', 'FormatController@store');
+Route::get('/format/{format}', 'FormatController@show');
+Route::get('/format/{format}/edit', 'FormatController@edit');
+Route::patch('/format/{format}', 'FormatController@update');
+Route::delete('/format/{format}', 'FormatController@destroy');
 
 /**
  * Category routes
  */
-Route::get('/categories', 'CategoriesController@index');
-Route::get('/categories/create', 'CategoriesController@create');
-Route::post('/categories', 'CategoriesController@store');
-Route::get('/categories/{category}', 'CategoriesController@show');
-Route::get('/categories/{category}/edit', 'CategoriesController@edit');
-Route::patch('/categories/{category}', 'CategoriesController@update');
-Route::delete('/categories/{category}', 'CategoriesController@destroy');
+Route::get('/category', 'CategoryController@index');
+Route::get('/category/create', 'CategoryController@create');
+Route::post('/category', 'CategoryController@store');
+Route::get('/category/{category}', 'CategoryController@show');
+Route::get('/category/{category}/edit', 'CategoryController@edit');
+Route::patch('/category/{category}', 'CategoryController@update');
+Route::delete('/category/{category}', 'CategoryController@destroy');
 
 /**
  * Extra Rate routes
  */
-Route::get('/extrarates', 'ExtraRatesController@index');
-Route::get('/extrarates/create', 'ExtraRatesController@create');
-Route::post('/extrarates', 'ExtraRatesController@store');
-Route::get('/extrarates/{extrarate}', 'ExtraRatesController@show');
-Route::get('/extrarates/{extrarate}/edit', 'ExtraRatesController@edit');
-Route::patch('/extrarates/{extrarate}', 'ExtraRatesController@update');
-Route::delete('/extrarates/{extrarate}', 'ExtraRatesController@destroy');
+Route::get('/extrarate', 'ExtraRateController@index');
+Route::get('/extrarate/create', 'ExtraRateController@create');
+Route::post('/extrarate', 'ExtraRateController@store');
+Route::get('/extrarate/{extrarate}', 'ExtraRateController@show');
+Route::get('/extrarate/{extrarate}/edit', 'ExtraRateController@edit');
+Route::patch('/extrarate/{extrarate}', 'ExtraRateController@update');
+Route::delete('/extrarate/{extrarate}', 'ExtraRateController@destroy');
 
 /**
  * Article routes
  */
-Route::get('/articles', 'ArticlesController@index');
-Route::get('/articles/create', 'ArticlesController@create');
-Route::post('/articles', 'ArticlesController@store');
-Route::get('/articles/{article}', 'ArticlesController@show');
-Route::get('/articles/{article}/edit', 'ArticlesController@edit');
-Route::patch('/articles/{article}', 'ArticlesController@update');
-Route::delete('/articles/{article}', 'ArticlesController@destroy');
+Route::get('/article', 'ArticleController@index');
+Route::get('/article/create', 'ArticleController@create');
+Route::post('/article', 'ArticleController@store');
+Route::get('/article/{article}', 'ArticleController@show');
+Route::get('/article/{article}/edit', 'ArticleController@edit');
+Route::patch('/article/{article}', 'ArticleController@update');
+Route::delete('/article/{article}', 'ArticleController@destroy');
 
 /**
  * Document routes
  */
-Route::get('/documents', 'DocumentsController@index');
-Route::get('/documents/create', 'DocumentsController@create');
-Route::post('/documents', 'DocumentsController@store');
-Route::get('/documents/{document}', 'DocumentsController@show');
-Route::get('/documents/{document}/edit', 'DocumentsController@edit');
-Route::patch('/documents/{document}', 'DocumentsController@update');
-Route::delete('/documents/{document}', 'DocumentsController@destroy');
+Route::get('/document', 'DocumentController@index');
+Route::get('/document/create', 'DocumentController@create');
+Route::post('/document', 'DocumentController@store');
+Route::get('/document/{document}', 'DocumentController@show');
+Route::get('/document/{document}/edit', 'DocumentController@edit');
+Route::patch('/document/{document}', 'DocumentController@update');
+Route::delete('/document/{document}', 'DocumentController@destroy');
 
 /**
  * Delivery routes
  */
-Route::get('/deliveries', 'DeliveriesController@index');
-Route::get('/deliveries/create', 'DeliveriesController@create');
-Route::post('/deliveries', 'DeliveriesController@store');
-Route::get('/deliveries/{delivery}', 'DeliveriesController@show');
-Route::get('/deliveries/{delivery}/edit', 'DeliveriesController@edit');
-Route::patch('/deliveries/{delivery}', 'DeliveriesController@update');
-Route::delete('/deliveries/{delivery}', 'DeliveriesController@destroy');
-
-/**
- * Option routes
- */
-Route::get('/options', 'OptionsController@index');
-Route::get('/options/create', 'OptionsController@create');
-Route::post('/options', 'OptionsController@store');
-Route::get('/options/{option}', 'OptionsController@show');
-Route::get('/options/{option}/edit', 'OptionsController@edit');
-Route::patch('/options/{option}', 'OptionsController@update');
-Route::delete('/options/{option}', 'OptionsController@destroy');
+Route::get('/delivery', 'DeliveryController@index');
+Route::get('/delivery/create', 'DeliveryController@create');
+Route::post('/delivery', 'DeliveryController@store');
+Route::get('/delivery/{delivery}', 'DeliveryController@show');
+Route::get('/delivery/{delivery}/edit', 'DeliveryController@edit');
+Route::patch('/delivery/{delivery}', 'DeliveryController@update');
+Route::delete('/delivery/{delivery}', 'DeliveryController@destroy');
