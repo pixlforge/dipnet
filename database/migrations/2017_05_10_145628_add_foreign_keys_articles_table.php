c07a80b755def243f8c5aec8bda3761c64ad7bba
Pixlforge <admin@pixlforge.ch>
2017-05-18 15:44:02 +0200

    Supprimé la FK sur table extra_rates qui n'existe plus

--- database/migrations/2017_05_10_145628_add_foreign_keys_articles_table.php
+++ database/migrations/2017_05_10_145628_add_foreign_keys_articles_table.php
@@ -15,7 +15,6 @@ class AddForeignKeysArticlesTable extends Migration
     {
         Schema::table('articles', function (Blueprint $table) {
             $table->foreign('category_id')->references('id')->on('categories');
-            $table->foreign('extra_rate_id')->references('id')->on('extra_rates');
         });
     }
 
@@ -28,7 +27,6 @@ class AddForeignKeysArticlesTable extends Migration
     {
         Schema::table('articles', function (Blueprint $table) {
             $table->dropForeign('articles_category_id_foreign');
-            $table->dropForeign('articles_extra_rate_id_foreign');
         });
     }
 }
