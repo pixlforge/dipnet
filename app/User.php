4969b71f0094a4aac86f24fd0b80eaf1259e6f51
Pixlforge <admin@pixlforge.ch>
2017-05-22 10:01:51 +0200

    Ajouté le trait Notifiable et extends Authenticatable

--- app/User.php
+++ app/User.php
@@ -63,7 +63,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'password',
+        'username', 'email', 'password', 'role', 'firstname', 'lastname',
     ];
 
     /**
@@ -72,7 +72,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password','remember_token',
     ];
     
 }
