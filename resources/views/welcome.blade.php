90df5ae252de9cbcf7c5285a5ae831f423766041
Pixlforge <admin@pixlforge.ch>
2017-05-22 16:25:37 +0200

    Titre dynamique

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="noindex, nofollow">
 
-        <title>Dipnet</title>
-
+        <title>{{ env('APP_NAME') }}</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
