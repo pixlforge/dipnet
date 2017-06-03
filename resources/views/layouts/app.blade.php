ee7717ec8e8b1f491e18b4421ce3854f3115f855
Pixlforge <admin@pixlforge.ch>
2017-06-03 14:20:44 +0200

    corrected jquery import style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,10 +32,7 @@
     @yield('content')
 
     <!-- Scripts -->
-    <script
-            src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
