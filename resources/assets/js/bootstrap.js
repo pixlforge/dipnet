8c12b573543ed0d4f8f8d06de33c13b21d06de89
Pixlforge <admin@pixlforge.ch>
2017-06-03 12:48:54 +0200

    switched from local jquery to cdn

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,9 +8,10 @@ window._ = require('lodash');
  */
 
 try {
-    window.$ = window.jQuery = require('jquery');
+    // window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
+
 } catch (e) {}
 
 /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,10 @@
     @yield('content')
 
     <!-- Scripts -->
+    <script
+            src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
