77e5e01b0ff4a2a8442432f857715b305c589a42
Pixlforge <admin@pixlforge.ch>
2017-06-07 14:40:17 +0200

    added debugbar service provider

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
 //        Laravel\Dusk\DuskServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
