46eab0d9444cd6a85821af496b1e1c432c38583b
Pixlforge <admin@pixlforge.ch>
2017-06-08 22:17:56 +0200

    added users card

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,13 +10,24 @@
             <div class="col-12">
                 <div class="card-columns">
 
+                    {{--Users--}}
+                    <div class="card">
+                        <div class="card-block">
+                            <h4 class="card-title">Utilisateurs</h4>
+                            <div class="list-group">
+                                <a href="{{ route('users') }}" class="list-group-item list-group-item-action">Tous les utilisateurs</a>
+                                <a href="{{ url('/users/create') }}" class="list-group-item list-group-item-action">Nouvel utilisateur</a>
+                            </div>
+                        </div>
+                    </div>
+
                     {{--Formats--}}
                     <div class="card">
                         <div class="card-block">
                             <h4 class="card-title">Formats</h4>
                             <div class="list-group">
-                                <a href="{{ url('/formats') }}" class="list-group-item list-group-item-action">Tous les formats</a>
-                                <a href="{{ url('/formats/create') }}" class="list-group-item list-group-item-action">Créer un format</a>
+                                <a href="{{ route('formats') }}" class="list-group-item list-group-item-action">Tous les formats</a>
+                                <a href="{{ url('/formats/create') }}" class="list-group-item list-group-item-action">Nouveau format</a>
                             </div>
                         </div>
                     </div>
@@ -26,8 +37,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Sociétés</h4>
                             <div class="list-group">
-                                <a href="{{ url('/companies') }}" class="list-group-item list-group-item-action">Toutes les sociétés</a>
-                                <a href="{{ url('/companies/create') }}" class="list-group-item list-group-item-action">Créer une société</a>
+                                <a href="{{ route('companies') }}" class="list-group-item list-group-item-action">Toutes les sociétés</a>
+                                <a href="{{ url('/companies/create') }}" class="list-group-item list-group-item-action">Nouvelle société</a>
                             </div>
                         </div>
                     </div>
@@ -37,8 +48,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Catégories</h4>
                             <div class="list-group">
-                                <a href="{{ url('/categories') }}" class="list-group-item list-group-item-action">Toutes les catégories</a>
-                                <a href="{{ url('/categories/create') }}" class="list-group-item list-group-item-action">Créer une catégorie</a>
+                                <a href="{{ route('categories') }}" class="list-group-item list-group-item-action">Toutes les catégories</a>
+                                <a href="{{ url('/categories/create') }}" class="list-group-item list-group-item-action">Nouvelle catégorie</a>
                             </div>
                         </div>
                     </div>
@@ -48,8 +59,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Affaires</h4>
                             <div class="list-group">
-                                <a href="{{ url('/businesses') }}" class="list-group-item list-group-item-action">Toutes les affaires</a>
-                                <a href="{{ url('/businesses/create') }}" class="list-group-item list-group-item-action">Créer une affaire</a>
+                                <a href="{{ route('businesses') }}" class="list-group-item list-group-item-action">Toutes les affaires</a>
+                                <a href="{{ url('/businesses/create') }}" class="list-group-item list-group-item-action">Nouvelle affaire</a>
                             </div>
                         </div>
                     </div>
@@ -59,8 +70,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Articles</h4>
                             <div class="list-group">
-                                <a href="{{ url('/articles') }}" class="list-group-item list-group-item-action">Tous les articles</a>
-                                <a href="{{ url('/articles/create') }}" class="list-group-item list-group-item-action">Créer un article</a>
+                                <a href="{{ route('articles') }}" class="list-group-item list-group-item-action">Tous les articles</a>
+                                <a href="{{ url('/articles/create') }}" class="list-group-item list-group-item-action">Nouvel article</a>
                             </div>
                         </div>
                     </div>
@@ -70,8 +81,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Contacts</h4>
                             <div class="list-group">
-                                <a href="{{ url('/contacts') }}" class="list-group-item list-group-item-action">Tous les contacts</a>
-                                <a href="{{ url('/contacts/create') }}" class="list-group-item list-group-item-action">Créer un contact</a>
+                                <a href="{{ route('contacts') }}" class="list-group-item list-group-item-action">Tous les contacts</a>
+                                <a href="{{ url('/contacts/create') }}" class="list-group-item list-group-item-action">Nouveau contact</a>
                             </div>
                         </div>
                     </div>
@@ -81,8 +92,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Commandes</h4>
                             <div class="list-group">
-                                <a href="{{ url('/orders') }}" class="list-group-item list-group-item-action">Toutes les commandes</a>
-                                <a href="{{ url('/orders/create') }}" class="list-group-item list-group-item-action">Créer une commande</a>
+                                <a href="{{ route('orders') }}" class="list-group-item list-group-item-action">Toutes les commandes</a>
+                                <a href="{{ url('/orders/create') }}" class="list-group-item list-group-item-action">Nouvelle commande</a>
                             </div>
                         </div>
                     </div>
@@ -92,8 +103,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Livraisons</h4>
                             <div class="list-group">
-                                <a href="{{ url('/deliveries') }}" class="list-group-item list-group-item-action">Toutes les livraisons</a>
-                                <a href="{{ url('/deliveries/create') }}" class="list-group-item list-group-item-action">Créer une livraison</a>
+                                <a href="{{ route('deliveries') }}" class="list-group-item list-group-item-action">Toutes les livraisons</a>
+                                <a href="{{ url('/deliveries/create') }}" class="list-group-item list-group-item-action">Nouvelle livraison</a>
                             </div>
                         </div>
                     </div>
@@ -103,8 +114,8 @@
                         <div class="card-block">
                             <h4 class="card-title">Documents</h4>
                             <div class="list-group">
-                                <a href="{{ url('/documents') }}" class="list-group-item list-group-item-action">Tous les documents</a>
-                                <a href="{{ url('/documents/create') }}" class="list-group-item list-group-item-action">Créer un document</a>
+                                <a href="{{ route('documents') }}" class="list-group-item list-group-item-action">Tous les documents</a>
+                                <a href="{{ url('/documents/create') }}" class="list-group-item list-group-item-action">Nouveau document</a>
                             </div>
                         </div>
                     </div>
