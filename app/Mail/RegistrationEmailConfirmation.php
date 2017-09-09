d317ef38d1d68ccd7bc510551bce8d27a3d13e29
Pixlforge <celien@pixlforge.ch>
2017-09-09 11:32:31 +0200

    Implement ShouldQueue behavior

--- app/Mail/RegistrationEmailConfirmation.php
+++ app/Mail/RegistrationEmailConfirmation.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class RegistrationEmailConfirmation extends Mailable
+class RegistrationEmailConfirmation extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
