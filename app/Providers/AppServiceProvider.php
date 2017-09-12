ce78ed369c93e8119c30c38cfb17cf08113a7fe8
Pixlforge <celien@pixlforge.ch>
2017-09-12 11:20:38 +0200

    Activate barryvdh/debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-//            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
 
         setlocale(LC_TIME, "fr_CH.utf8");
