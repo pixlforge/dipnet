7b84cc28e14d849d65a4e6bdc996fbb42029057d
Pixlforge <celien@pixlforge.ch>
2017-09-22 17:15:25 +0200

    Remove algolia integration

--- app/Company.php
+++ app/Company.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use SoftDeletes, Searchable;
+    use SoftDeletes;
 
     /**
      * Carbon dates.
--- resources/assets/js/components/Searchbar.vue
+++ resources/assets/js/components/Searchbar.vue
@@ -2,18 +2,11 @@
     <form class="form-inline form-nav"
           @submit.prevent>
         <i class="fa fa-search"></i>
-        <input type="text" id="orders" class="form-control" placeholder="Rechercher">
+        <input type="text" class="form-control" placeholder="Rechercher">
     </form>
 </template>
 
 <script>
-    import { orderautocomplete } from '../autocomplete';
-
     export default {
-        mounted() {
-            orderautocomplete('#orders');
-        },
-        methods: {
-        }
     }
 </script>
