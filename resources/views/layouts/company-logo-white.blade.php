a669f80c29cf22668aa594650aa01d3895daa50f
Pixlforge <celien@pixlforge.ch>
2017-09-26 08:39:41 +0200

    Refactor

--- resources/views/layouts/company-logo-white.blade.php
+++
@@ -1,9 +0,0 @@
-@if (env('APP_NAME') == 'Dipnet')
-    <a href="{{ route('index') }}">
-        <div class="company-logo-container company-logo-dip-white" aria-hidden="true"></div>
-    </a>
-@elseif (env('APP_NAME') == 'Multicop')
-    <a href="{{ route('index') }}">
-        <div class="company-logo-container company-logo-multicop-white" aria-hidden="true"></div>
-    </a>
-@endif
