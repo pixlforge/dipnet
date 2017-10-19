3a5c0b5cb53e7f0887ea567fdd17c078dfca48b1
Pixlforge <celien@pixlforge.ch>
2017-10-19 11:34:46 +0200

    Clean up

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -10,10 +10,3 @@ $factory->define(App\Company::class, function (Faker $faker) {
         'created_by_username' => $faker->userName,
     ];
 });
-
-$factory->state(App\Company::class, 'default', [
-    'name' => 'default',
-    'status' => 'temporaire',
-    'description' => null,
-    'created_by_username' => 'default',
-]);
