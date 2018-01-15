a9bc19376428a307bcbad727dc8f422ef3467b91
Pixlforge <celien@pixlforge.ch>
2018-01-15 15:18:41 +0100

    Removed Bootstrap and jQuery dependencies

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,11 +9,11 @@ window.Vue = require('vue');
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-  // window.$ = window.jQuery = require('./jquery-3.2.1');
-  require('bootstrap')
-} catch (e) {
-}
+// try {
+//   window.$ = window.jQuery = require('./jquery-3.2.1');
+//   require('bootstrap')
+// } catch (e) {
+// }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
