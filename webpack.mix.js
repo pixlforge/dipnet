26402dde5ba3887c6f692283adb8c6cf1fa47e66
Pixlforge <celien@pixlforge.ch>
2018-02-01 18:16:22 +0100

    Added multiple modes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,18 @@
 const { mix } = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/dip.scss', 'public/css')
-    .sass('resources/assets/sass/multicop.scss', 'public/css');
+  .sass('resources/assets/sass/app.scss', 'public/css');
+
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css')
+//    .browserSync({
+//      proxy: process.env.APP_URL
+//    });
+
+
+// mix.js('resources/assets/js/app.js', 'public/js')
+//   .sass('resources/assets/sass/app.scss', 'public/css')
+//   .browserSync({
+//     proxy: process.env.APP_URL
+//   })
+//   .version();
