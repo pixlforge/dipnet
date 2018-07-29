edaa6f216c6363beec7d6b5bf5e11d6bedbb47cd
Pixlforge <celien@pixlforge.ch>
2018-07-29 11:07:49 +0200

    Narrowed the fields needed in the response

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -8,6 +8,20 @@ use App\Http\Resources\UsersCollection;
 
 class UserController extends Controller
 {
+    private $fields = [
+        'id',
+        'username',
+        'role',
+        'email',
+        'is_solo',
+        'company_id',
+        'created_at',
+        'updated_at',
+        'email_confirmed',
+        'company_confirmed',
+        'contact_confirmed',
+    ];
+
     public function __construct()
     {
         $this->middleware('admin');
@@ -15,14 +29,18 @@ class UserController extends Controller
 
     public function index($sort = null)
     {
+        $users = [];
+
         if ($sort) {
             if ($sort === 'created_at' || $sort === 'role') {
-                return new UsersCollection(User::orderBy($sort, 'desc')->paginate(25));
+                $users = User::orderBy($sort, 'desc')->paginate(25, $this->fields);
             } else {
-                return new UsersCollection(User::orderBy($sort)->paginate(25));
+                $users = User::orderBy($sort)->paginate(25, $this->fields);
             }
         } else {
-            return new UsersCollection(User::orderBy('role', 'username')->paginate(25));
+            $users = User::orderBy('role', 'username')->paginate(25, $this->fields);
         }
+
+        return new UsersCollection($users);
     }
 }
