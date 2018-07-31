8373e7d4a7f2d0c2496e442d4b047ca784f62a11
Pixlforge <celien@pixlforge.ch>
2018-07-31 15:00:40 +0200

    Switched to dynamic route with parameters

--- resources/views/emails/confirm-email.blade.php
+++ resources/views/emails/confirm-email.blade.php
@@ -3,7 +3,7 @@
 
 Aidez-nous à déterminer que vous êtes bien un humain en confirmant la création de votre compte Dipnet à l'aide du bouton ci-dessous :)
 
-@component('mail::button', ['url' => url('/register/confirm?token=' . $user->confirmation_token), 'color' => 'red'])
+@component('mail::button', ['url' => url(route('register.confirm.index', ['token' => $user->confirmation_token])), 'color' => 'red'])
     Confirmer la création de mon compte
 @endcomponent
 
