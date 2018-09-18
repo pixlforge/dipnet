23bc2ebad14aa0ae5a0aeb4ea50ce16785e154e6
Pixlforge <celien@pixlforge.ch>
2018-09-18 11:57:26 +0200

    Changed email template text

--- resources/views/emails/user-updated-email-address.blade.php
+++ resources/views/emails/user-updated-email-address.blade.php
@@ -3,13 +3,15 @@
 
 Nous avons remarqué que vous aviez changé votre adresse e-mail.
 
-Veuillez, s'il-vous-plaît, confirmer que vous êtes bien le propriétaire de cette nouvelle adresse e-mail en accédant à l'adresse ci-dessous à l'aide de votre navigateur ou en cliquant sur le bouton ci-dessous:
+Veuillez confirmer que vous êtes bien le propriétaire de cette nouvelle adresse e-mail en accédant à l'adresse ci-dessous à l'aide de votre navigateur ou en cliquant sur le bouton:
+
+{{ route('register.confirm.index', ['token' => $user->confirmation_token]) }}
 
 @component('mail::button', ['url' => route('register.confirm.index', ['token' => $user->confirmation_token]), 'color' => 'red'])
 Confirmer ma nouvelle adresse e-mail
 @endcomponent
 
-Veuillez contacter l'administration dans le cas ou vous ne seriez par à l'origine de ce changement.
+Veuillez contacter l'administration dans le cas où vous ne seriez pas à l'origine de ce changement.
 
 Merci pour votre confiance,<br>
 {{ config('app.name') }}
